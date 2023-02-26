39de2a813b9bfd3814675f4a5ab008070e82ab32
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-02-26 13:37:02 +0330

    connect to DB

---
+++ CodeFirstDB/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using CodeFirstDB.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CodeFirstDB.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CodeFirstdb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<WeatherForecast> Forecasts { get; set; }
+
+    }
+}
